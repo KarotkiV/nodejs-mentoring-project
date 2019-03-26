e35c4cdfbcee1abac6bf6162ef25e5458b032ef5
Viachaslau_karotki@epam.com <Viachaslau_karotki@epam.com>
2019-03-26 10:50:50 +0300

    [VK] NodeJSMentoring - home work 5

--- appExp.js
+++ appExp.js
@@ -6,6 +6,10 @@ const app = express();
 const router = require('./routes/router.js');
 const cookieParse = require('./middlewares/cookie-parser.js'); 
 const queryParse = require('./middlewares/query-parser.js')
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
 
 app.use(cookieParse);
 
--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,60 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const jwt = require('jsonwebtoken');
 
-router.get('/api/products',(req,res) => {
+const user = {
+    'id' : 'id-example',
+    'login' : 'user',
+    'password' : 'password',
+    'firstName' : 'Viachaslau',
+    'lastName' : 'Karotki',
+    'email' : 'test@test.com'
+}
+
+const token = {
+    'id' : 'id-example',
+    'token' : 'token-example'
+}
+
+passport.use(new FacebookStrategy({
+    clientID: '1182863428552083',
+    callbackURL: 'http://localhost:9000/api/products'
+}, (token, refreshToken, profile, done) => {
+    userObject.facebookProfile = profile;
+    userObject.username = user.login;
+    userObject.email = user.email;
+    userObject.token = token;
+    done(null,user);
+}));
+
+passport.use(new LocalStrategy((userName,password,done) => {
+    if (user.login == userName && user.password == password) {
+        let genToken = jwt.sign({sub : user.id},token.token);
+        let userObject = {};
+        userObject.username = user.login;
+        userObject.email = user.email;
+        userObject.token = genToken;
+        done(null,userObject);
+    } else {
+        done(null, false, 'Bad credentials');
+    }
+}
+));
+
+router.get('/auth/facebook', passport.authenticate('facebook', (req,res) => {
+    let respObject = {};
+    respObject.code = '200';
+    respObject.message = 'OK';
+    respObject.data = {};
+    respObject.data.user = req.user;
+    res.json(respObject);
+}));
+
+router.get('/api/products', (req,res) => {
     res.send('All products');
 });
 
@@ -20,13 +72,17 @@ router.get('/api/products/:id/reviews', (req, res) => {
     res.send(`Review of product id = ${req.params.id}`);
 });
 
-router.post('/api/products', (req, res) => {
+router.post('/api/products', passport.authenticate('local', {session : false}), (req, res) => {
     const product = bodyParser.json();
     res.json(product);
 });
 
-router.get('/api/users', (req,res) => {
+router.get('/api/users', passport.authenticate('local', {session : false}), (req,res) => {
     res.send('All users');
+});
+
+router.post('/auth', (req,res) => {
+
 });
 
 module.exports = router;
